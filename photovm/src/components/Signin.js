57c15340a6e8ec4e504a9ce67174bacdde3bc12e
Vladimir <boxingvm16@gmail.com>
2020-01-13 17:58:26 +0200

    add sign-in form

--- photovm/src/components/Signin.js
+++ photovm/src/components/Signin.js
@@ -1,12 +1,33 @@
 import React, { useState } from 'react';
 import firebase from '../firebase/config';
-import {Auth } from '../context/authContext';
+import { Auth } from '../context/authContext';
 
 const Signin = () => {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {state, dispatch} = React.useContext(Auth);
+
+    const signin = (e) => {
+        e.preventDefault();
+        console.log("Form sent");
+    }
+
     return (
         <React.Fragment>
-                <p>Form</p>
+            <form onSubmit={signin}>
+                <p>Create an Account:</p>
+
+                <label htmlFor='email'>Email: </label>
+                <input type='email' name='email' onChange= {(e) => setEmail(e.target.value)} />
+
+                <label htmlFor='password'>Password: </label>
+                <input type='password' name='password' onChange= { (e) => setPassword(e.target.value) } />
+
+                <input type='submit' value="Create account"/>
+
+            </form>
         </React.Fragment>
 
     )
